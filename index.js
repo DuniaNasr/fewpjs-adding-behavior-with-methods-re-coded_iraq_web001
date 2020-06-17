65d4836125ebb376bb1fd5ce866c1bc34954725d
Dunia Nasr <dunia.nasr777@gmail.com>
2020-06-17 13:37:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+class Cat{
+constructor(name,sex){
+  this.name=name;
+  this.sex=sex;
+}
+speak(){
+  return ` ${this.name} says meow!`;
+}
+}
+class Dog{
+  constructor(name,sex){
+   this.name=name;
+  this.sex=sex;
+}
+speak(){
+  return name="woof";
+}
+}
+
+
+
+
+class Bird{
+ constructor(name,sex){
+   this.name=name;
+  this.sex=sex;
+}
+bird.speak(){
+  return "it's me";
+}
+
+}
+let cat=new Cat("pise","male");
+let dog=new Dog("pise","male");
+let bird=new Bird("pise","male");
+
+
+
+
+
+
+
+
